0e328c1cdbb949adfd3e6b635c32b29c6bc6097b
josephyao <dermott@126.com>
2013-11-03 22:58:47 +0800

    one word list and input is double of this word more than one word list and input is double of non first word

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -1,14 +1,29 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class Anagram {
 
+	private String[] wordList;
+
 	public Anagram(String[] wordList) {
+		this.wordList = wordList;
 	}
 
 	public List<String> generate(String input) {
+		if (wordList.length == 0)
+			return new ArrayList<String>();
+		
+		for (String anagram : wordList)
+			if (isAnagram(input, anagram))
+				return Arrays.asList(new String[]{anagram + " " + anagram});
+		
 		return new ArrayList<String>();
+	}
+
+	private boolean isAnagram(String input, String anagram) {
+		return (anagram + anagram).equals(input);
 	}
 
 }
--- src/test/java/TestAnagram.java
+++ src/test/java/TestAnagram.java
@@ -1,7 +1,7 @@
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -11,9 +11,27 @@ public class TestAnagram {
 
 	@Test
 	public void empty_word_list_and_input_is_any_word() {
-		Anagram generator = new Anagram(new String[]{});
-		List<String> actualAnagrams = generator.generate("anyword");
-		assertEquals(new ArrayList<String>(), actualAnagrams);
+		assertAnagramsEquals(new String[]{}, new String[]{}, "anyword");
 	}
 
+	@Test
+	public void one_word_list_and_input_is_double_of_this_word() {
+		assertAnagramsEquals(new String[]{"a a"}, new String[]{"a"}, "aa");
+		assertAnagramsEquals(new String[]{"b b"}, new String[]{"b"}, "bb");
+		assertAnagramsEquals(new String[]{}, new String[]{"b"}, "aa");
+		assertAnagramsEquals(new String[]{"ab ab"}, new String[]{"ab"}, "abab");
+	}
+	
+	@Test
+	public void more_than_one_word_list_and_input_is_double_of_non_first_word() {
+		assertAnagramsEquals(new String[]{"b b"}, new String[]{"a", "b"}, "bb");
+		assertAnagramsEquals(new String[]{"c c"}, new String[]{"a", "b", "c"}, "cc");
+	}
+
+	private void assertAnagramsEquals(String[] expectedAnagrams, String[] wordList, String input) {
+		Anagram generator = new Anagram(wordList);
+		List<String> actualAnagrams = generator.generate(input);
+		assertEquals(Arrays.asList(expectedAnagrams), actualAnagrams);
+	}
+	
 }
